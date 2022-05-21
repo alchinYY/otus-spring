06b573ed4c323b03e99b1599bac4e7e51546545f
17876388 <alchin.y.y@sberbank.ru>
2022-05-21 22:26:11 +0300

    fix tests

--- task-manager/src/test/java/ru/otus/spring/task/manager/web/integration/SecurityUtilTest.java
+++ task-manager/src/test/java/ru/otus/spring/task/manager/web/integration/SecurityUtilTest.java
@@ -4,16 +4,19 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.context.SecurityContextImpl;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import ru.otus.spring.task.manager.model.UserEntity;
 import ru.otus.spring.task.manager.service.impl.UserService;
 import ru.otus.spring.task.manager.web.SecurityUtil;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest
 @Import(UserService.class)
@@ -25,11 +28,16 @@ class SecurityUtilTest {
     private String userPass;
     @Autowired
     private SecurityUtil securityUtil;
-    @Autowired
+    @MockBean
     private UserService userService;
 
     @Test
     void getCurrentUser() {
+        UserEntity userEntityForTest = new UserEntity();
+        userEntityForTest.setLogin(userNameForTest);
+        userEntityForTest.setPassword(new BCryptPasswordEncoder().encode(userPass));
+        when(userService.loadUserByUsername(userNameForTest)).thenReturn(userEntityForTest);
+
         SecurityContext securityContext = new SecurityContextImpl();
         securityContext.setAuthentication(
                 new UsernamePasswordAuthenticationToken(userService.loadUserByUsername(userNameForTest), userPass)
